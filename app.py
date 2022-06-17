893347e73bf4bbf3533fbcc55bfd3b043009b9c5
yooonyng <yooonyng@gmail.com>
2022-06-17 15:52:37 +0900

    0617

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-
-from resources.recipe import RecipelistResource
+from resources.recipe import RecipeListResource
 
 app = Flask(__name__)
 api = Api(app)
 
 # 경로와 리소스(API 코드)를 연결한다.
-api.add_resource(RecipelistResource,'/recipes')
+api.add_resource(RecipeListResource,'/recipes')
 
 
 if __name__ == '__main__':
--- resources/recipe.py
+++ resources/recipe.py
@@ -14,7 +14,7 @@ import mysql.connector
 ## API를 만들기 위한 클래스는 flask_restful 라이브러리의
 ## Resource 클래스를 상속해서 만들어야한다.
 
-class RecipelistResource(Resource):
+class RecipeListResource(Resource):
     # restful api의 method에 해당하는 함수 작성
     def post(self):
         # api 실행 코드를 여기에 작성
@@ -59,3 +59,43 @@ class RecipelistResource(Resource):
 
         return {"result":"success"}, 200
 
+    def get(self):
+        # 쿼리 스트링으로 오는 데이터는 아래처럼 처리해준다.
+        offset = request.args.get('offset')
+        limit = request.args.get('limit')
+        
+        # DB로부터 데이터를 받아서 클라이언트에 보내준다.
+        try:
+            connection = get_connection()
+            query = '''select * from recipe
+                    limit '''+offset+''','''+limit+''';'''
+
+            # select문은 dictionary=True를 해준다
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query)
+            # select문은 아래 함수를 이용해서 데이터를 가져온다
+            result_list = cursor.fetchall()
+            print(result_list)
+            
+            # DB에서 가져온 timestamp는 파이썬의 datetime으로 자동 변경된다.
+            # 문제는 이 데이터를 json으로 바로 보낼 수 없으므로 
+            # 문자열로 바꿔서 다시 저장해서 보내야한다.
+            i = 0
+            for record in result_list:
+               result_list[i]['created_at'] = record['created_at'].isoformat()
+               result_list[i]['updated_at'] = record['updated_at'].isoformat()
+               i = i+1
+
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e:
+            print(e)
+            cursor.close()
+            connection.close()
+        
+            return {"error":str(e)}, 503
+
+        return {"result":"success",
+            "count":len(result_list),
+            "result_list":result_list}, 200
