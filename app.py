5e066f94e4dea767ca85c656906c47fcb232cef0
yooonyng <yooonyng@gmail.com>
2022-06-20 16:15:19 +0900

    0620

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from config import Config
 from resources.recipe import RecipeListResource
 from resources.recipe_info import RecipeResource
 from resources.recipe_publish import RecipePublishResource
 from resources.user import UserLoginResource, UserRegisterResource
 
 app = Flask(__name__)
+
+# 환경변수 셋팅하기
+app.config.from_object(Config)
+
+# JWT 토큰 라이브러리 만들기
+jwt = JWTManager(app)
+
 api = Api(app)
 
 # 경로와 리소스(API 코드)를 연결한다.
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    JWT_SECRET_KEY = 'yhacademy1029##hello'
+    JWT_ACCESS_TOKEN_EXPIRES = False
+    
